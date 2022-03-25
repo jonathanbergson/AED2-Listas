392e6ab6d26c68ba97afa45a8712deb37e894f06
Jonathan Bergson <contato@bergson.me>
2022-03-25 20:41:55 -0300

    chore: iniciado lista 4

---
+++ AED2_04/PlateMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AED2_04
+{
+    // NOTE: Cardapio
+    class PlateMenu
+    {
+    }
+}
