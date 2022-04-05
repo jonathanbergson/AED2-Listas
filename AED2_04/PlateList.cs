02630acc1436a388928bb300abd523565669b04b
Jonathan Bergson <contato@bergson.me>
2022-04-05 20:44:56 -0300

    lista 4

--- AED2_04/PlateList.cs
+++ AED2_04/PlateList.cs
@@ -8,5 +8,14 @@ namespace AED2_04
 {
     class PlateList
     {
+        private Plate _head;
+        private Plate _tail;
+
+        public PlateList()
+        {
+            Plate head = new Plate(0, "head", 0f);
+            _head = head;
+            _tail = head;
+        }
     }
 }
