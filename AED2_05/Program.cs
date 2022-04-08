0b91bee0a095cea6720be2a9d260ec607475bb1a
Jonathan Bergson <contato@bergson.me>
2022-04-07 23:20:51 -0300

    chore: adicionado PDF da lista

---
+++ AED2_05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AED2_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
